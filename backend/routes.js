e607711116a7b795b8e801f07381087289663a51
akshatdalton <akshat.dak@students.iiit.ac.in>
2021-12-22 15:51:51 +0530

    User: Added user authentication.

--- backend/routes.js
+++ backend/routes.js
@@ -1,12 +1,12 @@
 const express = require("express");
+const router = express.Router();
+const verifyToken = require("./middleware/verifyToken");
 
 const ProblemController = require("./controllers/ProblemController");
 const UserController = require("./controllers/UserController");
 
-const router = express.Router();
-
 // Handle everything related to Problem, TestCases and its Submission.
-router.get("/api/problems", ProblemController.getProblems);
+router.get("/api/problems", verifyToken, ProblemController.getProblems);
 router.post("/api/problems", ProblemController.createProblem);
 router.get("/api/problem/:id", ProblemController.getProblem);
 router.delete("/api/problem/:id", ProblemController.deleteProblem);
@@ -17,5 +17,6 @@ router.post("/api/submission", ProblemController.createSubmission);
 
 // Handle everything related to User.
 router.post("/api/users", UserController.createUser);
+router.post("/api/auth", UserController.authUser);
 
 module.exports = router;
