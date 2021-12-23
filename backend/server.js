aab5b84ece392b74b5510e0c70e1d5f4de879f80
akshatdalton <akshat.dak@students.iiit.ac.in>
2021-12-23 10:39:37 +0530

    production: Added script for `deployment`.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 require("dotenv").config();
 
@@ -25,6 +26,18 @@ app.use(cors());
 
 // Redirect all client requests to routes.js file.
 app.use(routes);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static("../frontend/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, "..", "frontend", "build", "index.html")
+        );
+    });
+}
 
 const port = process.env.PORT || 5000;
 
